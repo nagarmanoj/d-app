007aebb2a2604e2a80b10ec204c402f6acc9d5ae
nagarmanoj <nagar.manojkumar1999@gmail.com>
2024-02-19 17:42:36 +0530

    update file

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,9 @@
 import  mongoose from"mongoose";
 const dbConnect = async() =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGOODB_URL!);
+        const conn = await mongoose.connect('mongodb+srv://dhakar:Dhakar$8441066893@e-store.q7icw9s.mongodb.net/d-app');
         console.log("Database Connect Successfylly");
+        
     }catch (error){
         console.error(`Database error ${error}`);
         process.exit(1);
